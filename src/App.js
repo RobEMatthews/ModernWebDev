dc44f51d38f3a96c1d74ea22aac245f5bc4d9ad0
Robbie <repmatthews@gmail.com>
2022-07-14 18:11:50 -0400

    dark_mode

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import Components from "./Components/Components.js";
 import * as Env from "./environments.js";
 import Parse from "parse";
+import DarkMode from "./Components/DarkMode.tsx";
 
 Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
 Parse.serverURL = Env.SERVER_URL;
 
 function App() {
-  return <Components />;
+  return( 
+    <><div className="App">
+    <header className="App-header">
+      <DarkMode />
+    </header>
+  </div>
+  
+  <Components /></>);
 }
 
 export default App;
